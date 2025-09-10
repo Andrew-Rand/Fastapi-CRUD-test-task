6be660474f288ce90467fad74f6ef055b94d0971
Andrew <xseven693@gmail.com>
2025-09-10 13:26:36 +0300

    Add .env

--- app/common/db/engine.py
+++ app/common/db/engine.py
@@ -2,10 +2,10 @@ import os
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/articles_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
-    'postgresql+asyncpg://postgres:postgres@db/articles_db',
+    url=DATABASE_URL,
     echo=True,
     future=True,
 )
